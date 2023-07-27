f457cd1cdf6c3d11f35f8d5be6b3ee638c17597f
AnnaIllarionova <annailinas@gmail.com>
2023-07-27 13:43:56 +0300

    first commit

---
+++ components/add-post-page-component.js
@@ -0,0 +1,49 @@
+import { renderHeaderComponent } from "./header-component.js";
+import { renderUploadImageComponent } from "./upload-image-component.js";
+
+export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
+  const render = () => {
+    // TODO: Реализовать страницу добавления поста
+    const appHtml = `
+    <div class="page-container">
+      <div class="header-container"></div>
+      <h3 class="form-title">
+        Cтраница добавления поста
+      </h3>
+      <div class="form-inputs">
+        <div class="upload-image-container"></div>
+        <input type="text" id="comment-input" class="input" placeholder="Комментарий" />
+        <button class="button" id="add-button">Добавить</button>
+    </div>
+  `;
+
+    appEl.innerHTML = appHtml;
+
+    document.getElementById("add-button").addEventListener("click", () => {
+      const commentElement = document.getElementById("comment-input");
+      onAddPostClick({
+        description: commentElement.value,
+        imageUrl,
+      });
+      console.log(uploadImageContainer.value);
+      console.log(commentElement.value);
+    });
+  };
+
+  render();
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+
+  const uploadImageContainer = appEl.querySelector(".upload-image-container");
+  let imageUrl = "";
+
+  if (uploadImageContainer) {
+    renderUploadImageComponent({
+      element: appEl.querySelector(".upload-image-container"),
+      onImageUrlChange(newImageUrl) {
+        imageUrl = newImageUrl;
+      },
+    });
+  }
+}
---
+++ index.js
@@ -0,0 +1,181 @@
+import { getPosts, getUserPost, postNewPost } from "./api.js";
+import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
+import { renderAuthPageComponent } from "./components/auth-page-component.js";
+import {
+  ADD_POSTS_PAGE,
+  AUTH_PAGE,
+  LOADING_PAGE,
+  POSTS_PAGE,
+  USER_POSTS_PAGE,
+} from "./routes.js";
+import { renderPostsPageComponent } from "./components/posts-page-component.js";
+import { renderLoadingPageComponent } from "./components/loading-page-component.js";
+import {
+  getUserFromLocalStorage,
+  removeUserFromLocalStorage,
+  saveUserToLocalStorage,
+} from "./helpers.js";
+
+export let user = getUserFromLocalStorage();
+export let page = null;
+export let posts = [];
+
+const getToken = () => {
+  const token = user ? `Bearer ${user.token}` : undefined;
+  return token;
+};
+
+export const logout = () => {
+  user = null;
+  removeUserFromLocalStorage();
+  goToPage(POSTS_PAGE);
+};
+
+/**
+ * Включает страницу приложения
+ */
+export const goToPage = (newPage, data) => {
+  if (
+    [
+      POSTS_PAGE,
+      AUTH_PAGE,
+      ADD_POSTS_PAGE,
+      USER_POSTS_PAGE,
+      LOADING_PAGE,
+    ].includes(newPage)
+  ) {
+    if (newPage === ADD_POSTS_PAGE) {
+      // Если пользователь не авторизован, то отправляем его на авторизацию перед добавлением поста
+      page = user ? ADD_POSTS_PAGE : AUTH_PAGE;
+      return renderApp();
+    }
+
+    if (newPage === POSTS_PAGE) {
+      page = LOADING_PAGE;
+      renderApp();
+
+      return getPosts({ token: getToken() })
+        .then((newPosts) => {
+          page = POSTS_PAGE;
+          posts = newPosts;
+          renderApp();
+        })
+        .catch((error) => {
+          console.error(error);
+          goToPage(POSTS_PAGE);
+        });
+    }
+
+    if (newPage === USER_POSTS_PAGE) {
+      // TODO: реализовать получение постов юзера из API
+      console.log("Открываю страницу пользователя: ", data.userId);
+      page = USER_POSTS_PAGE;
+      const userId = data.userId;
+      posts = [];
+      posts = getUserPost({ token: getToken(), userId })
+      return renderApp();
+    }
+
+    page = newPage;
+    renderApp();
+
+    return;
+  }
+
+  throw new Error("страницы не существует");
+};
+
+const renderApp = () => {
+  const appEl = document.getElementById("app");
+  if (page === LOADING_PAGE) {
+    return renderLoadingPageComponent({
+      appEl,
+      user,
+      goToPage,
+    });
+  }
+
+  if (page === AUTH_PAGE) {
+    return renderAuthPageComponent({
+      appEl,
+      setUser: (newUser) => {
+        user = newUser;
+        saveUserToLocalStorage(user);
+        goToPage(POSTS_PAGE);
+      },
+      user,
+      goToPage,
+    });
+  }
+
+  if (page === ADD_POSTS_PAGE) {
+    return renderAddPostPageComponent({
+      appEl,
+      onAddPostClick({ description, imageUrl }) {
+        // TODO: реализовать добавление поста в API
+        postNewPost({ token: getToken(), description, imageUrl });
+        console.log("Добавляю пост...", { description, imageUrl });
+        goToPage(POSTS_PAGE);
+      },
+    });
+  }
+
+  if (page === POSTS_PAGE) {
+    return renderPostsPageComponent({
+      appEl,
+    });
+  }
+
+  if (page === USER_POSTS_PAGE) {
+    // TODO: реализовать страницу фотографий пользвателя
+    
+    getUserPost({ token: getToken()})
+    .then((userPosts) => {
+      const userPostHtml = userPosts.map((post) => {
+        return `
+        <li class="post">
+          <div class="post-header" data-user-id="${post.user.id}">
+              <img src="${post.user.imageUrl}" class="post-header__user-image">
+              <p class="post-header__user-name">${post.user.name}</p>
+          </div>
+          <div class="post-image-container">
+            <img class="post-image" src="${post.imageUrl}">
+          </div>
+          <div class="post-likes">
+            <button data-post-id="${post.id}" class="like-button">
+              <img src="./assets/images/like-active.svg">
+            </button>
+            <p class="post-likes-text">
+              Нравится: <strong>2</strong>
+            </p>
+          </div>
+          <p class="post-text">
+            <span class="user-name">${post.user.name}</span>
+            ${post.description}
+          </p>
+          <p class="post-date">
+            19 минут назад
+          </p>
+        </li>
+        `}).join('');
+
+        const userPostsHtml = `
+        <div class="page-container">
+          <div class="header-container"></div>
+          <ul class="posts">
+            ${userPostHtml}
+          </ul>
+        </div>
+        `;
+
+        appEl.innerHTML = userPostsHtml;
+    })
+    .catch((error) => {
+      console.warn('error');
+      appEl.innerHTML = 'Ошибка при загрузке страницы пользователя';
+    })
+   return;
+  }
+};
+
+goToPage(POSTS_PAGE);
